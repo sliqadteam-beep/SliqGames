d20cec58f12f32f34537e2a60167d43e26cbccc4
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-09 18:42:42 +0200

    Add SliqServer Android app build config

---
+++ android/SliqServer/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "org.sliqado.sliqserver"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.sliqado.sliqserver"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
